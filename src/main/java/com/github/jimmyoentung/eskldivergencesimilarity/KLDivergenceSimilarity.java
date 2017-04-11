fd1eb5c06c4ac3a107205a31bd0911f7d365fae8
jimmyoentung <jimmyoentung@gmail.com>
2017-04-11 10:25:46 +1000

    initial update

--- src/main/java/com/github/jimmyoentung/eskldivergencesimilarity/KLDivergenceSimilarity.java
+++ src/main/java/com/github/jimmyoentung/eskldivergencesimilarity/KLDivergenceSimilarity.java
@@ -1,66 +1,4 @@
-package com.github.hscells.customsimilarity;
-
-/*
-import org.apache.lucene.index.FieldInvertState;
-import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.search.CollectionStatistics;
-import org.apache.lucene.search.TermStatistics;
-import org.apache.lucene.search.similarities.Normalization;
-import org.apache.lucene.search.similarities.Similarity;
-
-import org.apache.lucene.search.similarities.BM25Similarity;
-
-
-import java.io.IOException;
-
-//
-// * Created by Harry Scells on 6/4/17.
-//
-
-class MyCustomSimilarity extends Similarity {
-
-    private Normalization normalization;
-
-    MyCustomSimilarity(Normalization normalization) {
-        this.normalization = normalization;
-    }
-
-    @Override
-    public long computeNorm(FieldInvertState state) {
-        return 0;
-    }
-
-    @Override
-    public SimWeight computeWeight(CollectionStatistics collectionStats, TermStatistics... termStats) {
-        return null;
-    }
-
-    @Override
-    public SimScorer simScorer(SimWeight weight, LeafReaderContext context) throws IOException {
-        return null;
-    }
-}
-*/
-
-
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
+package com.github.jimmyoentung.eskldivergencesimilarity;
 
 import java.util.List;
 import java.util.Locale;
@@ -68,6 +6,11 @@ import java.util.Locale;
 import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.similarities.LMSimilarity;
 import org.apache.lucene.search.similarities.BasicStats;
+
+//
+// Created by Harry Scells on 6/4/17.
+// Modified by Jimmy on 11 Apr 2017
+//
 
 /**
  * Bayesian smoothing using Dirichlet priors. From Chengxiang Zhai and John
@@ -84,28 +27,28 @@ import org.apache.lucene.search.similarities.BasicStats;
  *
  * @lucene.experimental
  */
-public class MyCustomSimilarity extends LMSimilarity {
+public class KLDivergenceSimilarity extends LMSimilarity {
     /** The &mu; parameter. */
     private final float mu;
 
     /** Instantiates the similarity with the provided &mu; parameter. */
-    public MyCustomSimilarity(CollectionModel collectionModel, float mu) {
+    public KLDivergenceSimilarity(CollectionModel collectionModel, float mu) {
         super(collectionModel);
         this.mu = mu;
     }
 
     /** Instantiates the similarity with the provided &mu; parameter. */
-    public MyCustomSimilarity(float mu) {
+    public KLDivergenceSimilarity(float mu) {
         this.mu = mu;
     }
 
     /** Instantiates the similarity with the default &mu; value of 2000. */
-    public MyCustomSimilarity(CollectionModel collectionModel) {
+    public KLDivergenceSimilarity(CollectionModel collectionModel) {
         this(collectionModel, 2000);
     }
 
     /** Instantiates the similarity with the default &mu; value of 2000. */
-    public MyCustomSimilarity() {
+    public KLDivergenceSimilarity() {
         this(2000);
     }
 
---
+++ src/main/java/com/github/jimmyoentung/eskldivergencesimilarity/KLDivergenceSimilarityProvider.java
@@ -0,0 +1,41 @@
+package com.github.jimmyoentung.eskldivergencesimilarity;
+
+//
+// Created by Harry Scells on 6/4/17.
+// Modified by Jimmy on 11 Apr 2017
+//
+
+
+import org.apache.lucene.search.similarities.Similarity;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.index.similarity.AbstractSimilarityProvider;
+
+
+
+/**
+ * {@link SimilarityProvider} for {@link LMDirichletSimilarity}.
+ * <p>
+ * Configuration options available:
+ * <ul>
+ *     <li>mu</li>
+ * </ul>
+ * @see LMDirichletSimilarity For more information about configuration
+ */
+public class KLDivergenceSimilarityProvider extends AbstractSimilarityProvider {
+
+    private final KLDivergenceSimilarity similarity;
+
+    public KLDivergenceSimilarityProvider(String name, Settings settings) {
+        super(name);
+        float mu = settings.getAsFloat("mu", 2000f);
+        this.similarity = new KLDivergenceSimilarity(mu);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Similarity get() {
+        return similarity;
+    }
+}
